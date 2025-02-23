7664306cdf2a88f2c43aa5cefd6e904ec6f6eae2
UyenNguyen-25 <khoang8666@gmail.com>
2025-02-23 23:47:54 +0700

    fix detail calendar

--- src/pages/DetailCalendar.jsx
+++ src/pages/DetailCalendar.jsx
@@ -113,10 +113,10 @@ function DetailCalendar({ events, visible, onClose }) {
       onCancel={onClose}
       footer={null}
       width={1000}
-      style={{ top: "8%", overflow: "visible" }}
+      centered
       styles={{
         body: {
-          // maxHeight: "78vh",
+          maxHeight: "78vh",
           padding: "5px 50px",
         },
       }}>
@@ -126,10 +126,8 @@ function DetailCalendar({ events, visible, onClose }) {
         <Tabs
           defaultActiveKey="1"
           type="card"
-          size={"middle"}
           style={{
             width: "100%",
-            height: "100%",
             fontWeight: 400,
           }}
           items={formattedItem}
@@ -314,7 +312,7 @@ const AppointmentDetailContent = ({ appointment }) => {
   ];
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 max-h-[69vh] overflow-auto">
       <h2 className="text-xl font-bold">Appointment Details</h2>
 
       {/* <Card title="Appointment Status" className="bg-gray-50">
